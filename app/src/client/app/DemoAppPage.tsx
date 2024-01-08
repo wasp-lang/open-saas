1ee0d119031a01d5d937b4018fd183ea1fbb35f6
Boris Martinovic <boris@martinovic.dev>
2024-01-08 19:57:28 +0100

    added prettier

--- app/src/client/app/DemoAppPage.tsx
+++ app/src/client/app/DemoAppPage.tsx
@@ -11,19 +11,19 @@ import { XSquare } from 'lucide-react';
 
 export default function DemoAppPage() {
   return (
-    <div className="my-10 lg:mt-20">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-4xl text-center">
-          <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl dark:text-white">
-            Create your AI-powered <span className="text-yellow-500">SaaS</span>
+    <div className='my-10 lg:mt-20'>
+      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+        <div className='mx-auto max-w-4xl text-center'>
+          <h2 className='mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl dark:text-white'>
+            Create your AI-powered <span className='text-yellow-500'>SaaS</span>
           </h2>
         </div>
-        <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600 dark:text-white">
+        <p className='mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600 dark:text-white'>
           Below is an example of integrating the OpenAI API into your SaaS.
         </p>
         {/* begin AI-powered Todo List */}
-        <div className="my-8 border rounded-3xl border-gray-900/10">
-          <div className="sm:w-[90%] md:w-[70%] lg:w-[50%] py-10 px-6 mx-auto my-8 space-y-10">
+        <div className='my-8 border rounded-3xl border-gray-900/10'>
+          <div className='sm:w-[90%] md:w-[70%] lg:w-[50%] py-10 px-6 mx-auto my-8 space-y-10'>
             <NewTaskForm handleCreateTask={createTask} />
           </div>
         </div>
@@ -39,11 +39,17 @@ function Todo({ id, isDone, description, time }: TodoProps) {
   const handleCheckboxChange = async (
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    await updateTask({ id, isDone: e.currentTarget.checked });
+    await updateTask({
+      id,
+      isDone: e.currentTarget.checked,
+    });
   };
 
   const handleTimeChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    await updateTask({ id, time: e.currentTarget.value });
+    await updateTask({
+      id,
+      time: e.currentTarget.value,
+    });
   };
 
   const handleDeleteClick = async () => {
@@ -51,12 +57,12 @@ function Todo({ id, isDone, description, time }: TodoProps) {
   };
 
   return (
-    <div className="flex items-center justify-between bg-purple-50 rounded-lg border border-gray-200 p-2 w-full">
-      <div className="flex items-center justify-between gap-5 w-full">
-        <div className="flex items-center gap-3">
+    <div className='flex items-center justify-between bg-purple-50 rounded-lg border border-gray-200 p-2 w-full'>
+      <div className='flex items-center justify-between gap-5 w-full'>
+        <div className='flex items-center gap-3'>
           <input
-            type="checkbox"
-            className="ml-1 form-checkbox bg-purple-300 checked:bg-purple-300 rounded border-purple-400 duration-200 ease-in-out hover:bg-purple-400 hover:checked:bg-purple-600 focus:ring focus:ring-purple-300 focus:checked:bg-purple-400 focus:ring-opacity-50 text-black"
+            type='checkbox'
+            className='ml-1 form-checkbox bg-purple-300 checked:bg-purple-300 rounded border-purple-400 duration-200 ease-in-out hover:bg-purple-400 hover:checked:bg-purple-600 focus:ring focus:ring-purple-300 focus:checked:bg-purple-400 focus:ring-opacity-50 text-black'
             checked={isDone}
             onChange={handleCheckboxChange}
           />
@@ -68,10 +74,10 @@ function Todo({ id, isDone, description, time }: TodoProps) {
             {description}
           </span>
         </div>
-        <div className="flex items-center gap-2">
+        <div className='flex items-center gap-2'>
           <input
-            id="time"
-            type="number"
+            id='time'
+            type='number'
             min={0.5}
             step={0.5}
             className={`w-18 h-8 text-center text-slate-600 text-xs rounded border border-gray-200 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-purple-300 focus:ring-opacity-50 ${
@@ -89,9 +95,9 @@ function Todo({ id, isDone, description, time }: TodoProps) {
           </span>
         </div>
       </div>
-      <div className="flex items-center justify-end w-15">
-        <button className="p-1" onClick={handleDeleteClick} title="Remove task">
-          <XSquare size="20" className="text-red-600 hover:text-red-700" />
+      <div className='flex items-center justify-end w-15'>
+        <button className='p-1' onClick={handleDeleteClick} title='Remove task'>
+          <XSquare size='20' className='text-red-600 hover:text-red-700' />
         </button>
       </div>
     </div>
@@ -127,7 +133,9 @@ function NewTaskForm({
   const handleGeneratePlan = async () => {
     try {
       setIsPlanGenerating(true);
-      const response = await generateGptResponse({ hours: todaysHours });
+      const response = await generateGptResponse({
+        hours: todaysHours,
+      });
       if (response) {
         console.log('response', response);
         setResponse(JSON.parse(response));
@@ -140,14 +148,14 @@ function NewTaskForm({
   };
 
   return (
-    <div className="flex flex-col justify-center gap-10">
-      <div className="flex flex-col gap-3">
-        <div className="flex items-center justify-between gap-3">
+    <div className='flex flex-col justify-center gap-10'>
+      <div className='flex flex-col gap-3'>
+        <div className='flex items-center justify-between gap-3'>
           <input
-            type="text"
-            id="description"
-            className="text-sm text-gray-600 w-full rounded-md border border-gray-200 bg-[#f5f0ff] shadow-md focus:outline-none focus:border-transparent focus:shadow-none duration-200 ease-in-out hover:shadow-none"
-            placeholder="Enter task description"
+            type='text'
+            id='description'
+            className='text-sm text-gray-600 w-full rounded-md border border-gray-200 bg-[#f5f0ff] shadow-md focus:outline-none focus:border-transparent focus:shadow-none duration-200 ease-in-out hover:shadow-none'
+            placeholder='Enter task description'
             value={description}
             onChange={(e) => setDescription(e.currentTarget.value)}
             onKeyDown={(e) => {
@@ -157,19 +165,19 @@ function NewTaskForm({
             }}
           />
           <button
-            type="button"
+            type='button'
             onClick={handleSubmit}
-            className="min-w-[7rem] font-medium text-gray-800/90 bg-yellow-50 shadow-md ring-1 ring-inset ring-slate-200 py-2 px-4 rounded-md hover:bg-yellow-100 duration-200 ease-in-out focus:outline-none focus:shadow-none hover:shadow-none"
+            className='min-w-[7rem] font-medium text-gray-800/90 bg-yellow-50 shadow-md ring-1 ring-inset ring-slate-200 py-2 px-4 rounded-md hover:bg-yellow-100 duration-200 ease-in-out focus:outline-none focus:shadow-none hover:shadow-none'
           >
             Add Task
           </button>
         </div>
       </div>
 
-      <div className="space-y-10 col-span-full">
+      <div className='space-y-10 col-span-full'>
         {isTasksLoading && <div>Loading...</div>}
         {tasks!! && tasks.length > 0 ? (
-          <div className="space-y-4">
+          <div className='space-y-4'>
             {tasks.map((task: Task) => (
               <Todo
                 key={task.id}
@@ -179,21 +187,21 @@ function NewTaskForm({
                 time={task.time}
               />
             ))}
-            <div className="flex flex-col gap-3">
-              <div className="flex items-center justify-between gap-3">
+            <div className='flex flex-col gap-3'>
+              <div className='flex items-center justify-between gap-3'>
                 <label
-                  htmlFor="time"
-                  className="text-sm text-gray-600 dark:text-gray-300 text-nowrap font-semibold"
+                  htmlFor='time'
+                  className='text-sm text-gray-600 dark:text-gray-300 text-nowrap font-semibold'
                 >
                   How many hours will you work today?
                 </label>
                 <input
-                  type="number"
-                  id="time"
+                  type='number'
+                  id='time'
                   step={0.5}
                   min={1}
                   max={24}
-                  className="min-w-[7rem] text-gray-800/90 text-center font-medium rounded-md border border-gray-200 bg-yellow-50 hover:bg-yellow-100 shadow-md focus:outline-none focus:border-transparent focus:shadow-none duration-200 ease-in-out hover:shadow-none"
+                  className='min-w-[7rem] text-gray-800/90 text-center font-medium rounded-md border border-gray-200 bg-yellow-50 hover:bg-yellow-100 shadow-md focus:outline-none focus:border-transparent focus:shadow-none duration-200 ease-in-out hover:shadow-none'
                   value={todaysHours}
                   onChange={(e) => setTodaysHours(e.currentTarget.value)}
                 />
@@ -201,19 +209,19 @@ function NewTaskForm({
             </div>
           </div>
         ) : (
-          <div className="text-gray-600 text-center">Add tasks to begin</div>
+          <div className='text-gray-600 text-center'>Add tasks to begin</div>
         )}
       </div>
 
       <button
-        type="button"
+        type='button'
         disabled={isPlanGenerating || tasks?.length === 0}
         onClick={() => handleGeneratePlan()}
-        className="flex items-center justify-center min-w-[7rem] font-medium text-gray-800/90 bg-yellow-50 shadow-md ring-1 ring-inset ring-slate-200 py-2 px-4 rounded-md hover:bg-yellow-100 duration-200 ease-in-out focus:outline-none focus:shadow-none hover:shadow-none disabled:opacity-70 disabled:cursor-not-allowed"
+        className='flex items-center justify-center min-w-[7rem] font-medium text-gray-800/90 bg-yellow-50 shadow-md ring-1 ring-inset ring-slate-200 py-2 px-4 rounded-md hover:bg-yellow-100 duration-200 ease-in-out focus:outline-none focus:shadow-none hover:shadow-none disabled:opacity-70 disabled:cursor-not-allowed'
       >
         {isPlanGenerating ? (
           <>
-            <CgSpinner className="inline-block mr-2 animate-spin" />
+            <CgSpinner className='inline-block mr-2 animate-spin' />
             Generating...
           </>
         ) : (
@@ -222,8 +230,8 @@ function NewTaskForm({
       </button>
 
       {!!response && (
-        <div className="flex flex-col">
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+        <div className='flex flex-col'>
+          <h3 className='text-lg font-semibold text-gray-900 dark:text-white'>
             Today's Schedule
           </h3>
 
@@ -236,11 +244,11 @@ function NewTaskForm({
 
 function TaskTable({ schedule }: { schedule: any[] }) {
   return (
-    <div className="flex flex-col gap-6 py-6">
+    <div className='flex flex-col gap-6 py-6'>
       {schedule.map((task: any) => (
         <table
           key={task.name}
-          className="table-auto w-full border-separate border border-spacing-2 rounded-md border-slate-200 shadow-sm"
+          className='table-auto w-full border-separate border border-spacing-2 rounded-md border-slate-200 shadow-sm'
         >
           <thead>
             <tr>
@@ -254,14 +262,14 @@ function TaskTable({ schedule }: { schedule: any[] }) {
                 }`}
               >
                 <span>{task.name}</span>
-                <span className="opacity-70 text-xs font-medium italic">
+                <span className='opacity-70 text-xs font-medium italic'>
                   {' '}
                   {task.priority} priority
                 </span>
               </th>
             </tr>
           </thead>
-          <tbody className="">
+          <tbody className=''>
             {task.subtasks.map((subtask: { description: any; time: any }) => (
               <tr>
                 <td
@@ -311,8 +319,8 @@ function Subtask({ description, time }: { description: string; time: number }) {
   return (
     <>
       <input
-        type="checkbox"
-        className="ml-1 form-checkbox bg-purple-500 checked:bg-purple-300 rounded border-purple-600 duration-200 ease-in-out hover:bg-purple-600 hover:checked:bg-purple-600 focus:ring focus:ring-purple-300 focus:checked:bg-purple-400 focus:ring-opacity-50"
+        type='checkbox'
+        className='ml-1 form-checkbox bg-purple-500 checked:bg-purple-300 rounded border-purple-600 duration-200 ease-in-out hover:bg-purple-600 hover:checked:bg-purple-600 focus:ring focus:ring-purple-300 focus:checked:bg-purple-400 focus:ring-opacity-50'
         checked={isDone}
         onChange={(e) => setIsDone(e.currentTarget.checked)}
       />
