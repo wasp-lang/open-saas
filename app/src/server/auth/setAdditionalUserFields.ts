31a9b021f1d1fa431c74b6fbb87db3e48a5e1798
vincanger <70215737+vincanger@users.noreply.github.com>
2024-02-15 13:48:43 +0100

    fix migration to Email auth

--- app/src/server/auth/setAdditionalUserFields.ts
+++ app/src/server/auth/setAdditionalUserFields.ts
@@ -1,10 +1,13 @@
 import { defineAdditionalSignupFields } from '@wasp/auth/index.js'
 
 export default defineAdditionalSignupFields({
+  // username: (data) => {
+  //   return data.email as string
+  // },
   isAdmin: (data) => {
     if (!data.email) {
       return false;
-    } 
+    }
     const adminEmails = process.env.ADMIN_EMAILS?.split(',') || [];
     return adminEmails.includes(data.email as string);
   },
