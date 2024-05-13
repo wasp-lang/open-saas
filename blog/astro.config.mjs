03ca9d570085cbd28f916cdffa0b16a9defa5a4a
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-05-13 16:16:14 +0200

    Updates Astro, Starlight, Starlight Blog versions (#122)
    * Updates Astro, Starlight, Starlight Blog versions
    
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>
    
    * Fixes formatting
    
    * Cleanup
    
    ---------
    
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -2,25 +2,12 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightBlog from 'starlight-blog';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://opensaas.sh',
   integrations: [
-    starlightBlog({
-      title: 'Blog',
-      customCss: ['./src/styles/tailwind.css'],
-      authors: {
-        vince: {
-          name: 'Vince',
-          title: 'Dev Rel @ Wasp',
-          picture: '/CRAIG_ROCK.png',
-          // Images in the `public` directory are supported.
-          url: 'https://wasp-lang.dev',
-        },
-      },
-    }),
     starlight({
       title: 'Your SaaS',
       customCss: ['./src/styles/tailwind.css'],
@@ -53,9 +40,6 @@ export default defineConfig({
       },
       components: {
         SiteTitle: './src/components/MyHeader.astro',
-        MarkdownContent: 'starlight-blog/overrides/MarkdownContent.astro',
-        Sidebar: 'starlight-blog/overrides/Sidebar.astro',
-        // ThemeSelect: 'starlight-blog/overrides/ThemeSelect.astro',
       },
 
       social: {
@@ -82,6 +66,20 @@ export default defineConfig({
             },
           ],
         },
+      ],
+      plugins: [
+        starlightBlog({
+          title: 'Blog',
+          customCss: ['./src/styles/tailwind.css'],
+          authors: {
+            vince: {
+              name: 'Vince',
+              title: 'Dev Rel @ Wasp',
+              picture: '/CRAIG_ROCK.png', // Images in the `public` directory are supported.
+              url: 'https://wasp-lang.dev',
+            },
+          },
+        }),
       ],
     }),
     tailwind({ applyBaseStyles: false }),
