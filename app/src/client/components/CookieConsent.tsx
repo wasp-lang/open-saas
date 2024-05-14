7a5b0f03742834f837b1f3dbd63c445682dae977
vincanger <70215737+vincanger@users.noreply.github.com>
2024-05-14 18:24:07 +0200

    get cookie banner working

---
+++ app/src/client/components/CookieConsent.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import * as CookieConsent from 'vanilla-cookieconsent';
+import 'vanilla-cookieconsent/dist/cookieconsent.css';
+import getConfig from './CookieConsentConfig';
+
+/**
+ * NOTE: if you do not want to use the cookie consent banner, you should first
+ * run `npm uninstall vanilla-cookieconsent`, and delete this component
+ * as well as its import in src/client/App.tsx
+ */
+const CookieConsentBanner = () => {
+  useEffect(() => {
+    CookieConsent.run(getConfig());
+  }, []);
+
+  return (
+    <div id='cookieconsent'></div>
+  );
+};
+
+export default CookieConsentBanner;
