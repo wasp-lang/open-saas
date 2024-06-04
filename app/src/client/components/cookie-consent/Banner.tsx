7ae3835743d632fd235d92ddd138f9dc44d61950
vincanger <70215737+vincanger@users.noreply.github.com>
2024-06-04 21:03:09 +0200

    Update app/src/client/components/cookie-consent/Banner.tsx
    Co-authored-by: Martin Šošić <Martinsos@users.noreply.github.com>

--- app/src/client/components/cookie-consent/Banner.tsx
+++ app/src/client/components/cookie-consent/Banner.tsx
@@ -4,7 +4,7 @@ import 'vanilla-cookieconsent/dist/cookieconsent.css';
 import getConfig from './Config';
 
 /**
- * NOTE: if you do not want to use the cookie consent banner, you should first
+ * NOTE: if you do not want to use the cookie consent banner, you should 
  * run `npm uninstall vanilla-cookieconsent`, and delete this component, its config file,
  * as well as its import in src/client/App.tsx
  */
