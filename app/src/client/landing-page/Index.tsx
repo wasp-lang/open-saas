225851aaf053504e82e69fa354d1f2f86f8efea0
vincanger <70215737+vincanger@users.noreply.github.com>
2024-06-05 11:18:00 +0200

    small landingpage changes

--- app/src/client/landing-page/Index.tsx
+++ app/src/client/landing-page/Index.tsx
@@ -9,21 +9,14 @@ import Footer from './Footer';
 export default function LandingPage() {
   return (
     <div className='bg-white dark:text-white dark:bg-boxdark-2'>
-      {/* Header */}
       <Header />
       <main className='isolate dark:bg-boxdark-2'>
-        {/* Hero section */}
         <Hero />
-        {/* Clients section */}
         <Clients />
-        {/* Feature section */}
         <Feature />
-        {/* Testimonial section */}
         <Testimonial />
-        {/* FAQ */}
         <Faq />
       </main>
-      {/* Footer */}
       <Footer />
     </div>
   );
