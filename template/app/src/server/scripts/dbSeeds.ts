503027bd5992fa4075a8452dc2147ee83d2c2c30
vincanger <70215737+vincanger@users.noreply.github.com>
2024-06-28 16:10:14 +0200

    cleaner seed script

--- template/app/src/server/scripts/dbSeeds.ts
+++ template/app/src/server/scripts/dbSeeds.ts
@@ -3,40 +3,44 @@ import { faker } from '@faker-js/faker';
 import type { PrismaClient } from '@prisma/client';
 import { TierIds } from '../../shared/constants.js';
 
+type MockUserData = Omit<User, 'id'>;
+
 /**
  * This function, which we've imported in `app.db.seeds` in the `main.wasp` file,
  * seeds the database with mock users via the `wasp db seed` command.
  * For more info see: https://wasp-lang.dev/docs/data-model/backends#seeding-the-database
  */
 export async function seedMockUsers(prismaClient: PrismaClient) {
-  await Promise.all(generateMockUsersData(50).map(
-    data => prismaClient.user.create({ data })
-  ));
+  await Promise.all(generateMockUsersData(50).map((data) => 
+    prismaClient.user.create({ data }))
+  );
 }
 
-function generateMockUsersData(numOfUsers: number) {
+function generateMockUsersData(numOfUsers: number): MockUserData[] {
   return faker.helpers.multiple(generateMockUserData, { count: numOfUsers });
 }
 
-function generateMockUserData() {
+function generateMockUserData(): MockUserData {
   const firstName = faker.person.firstName();
   const lastName = faker.person.lastName();
+  const subscriptionStatus = faker.helpers.arrayElement(['active', 'canceled', 'past_due', 'deleted', null]);
   const now = new Date();
-  const recently = faker.date.recent({ refDate: now }); // Creates a random date up to 24 hrs ago from refDate
-  const past = faker.date.past({ refDate: now }); // Creates a random date in past up to 1 year from refDate
-  const user = {
+  const createdAt = faker.date.past({ refDate: now });
+  const lastActiveTimestamp = faker.date.between({ from: createdAt, to: now });
+  const credits = subscriptionStatus ? 0 : faker.number.int({ min: 0, max: 10 });
+  const hasUserPaidOnStripe = !!subscriptionStatus || credits > 3 
+  return {
     email: faker.internet.email({ firstName, lastName }),
     username: faker.internet.userName({ firstName, lastName }),
-    createdAt: past,
-    lastActiveTimestamp: recently,
+    createdAt,
+    lastActiveTimestamp,
     isAdmin: false,
-    stripeId: `cus_${faker.string.uuid()}`,
     sendEmail: false,
-    subscriptionStatus: faker.helpers.arrayElement(['active', 'canceled', 'past_due', 'deleted', null]),
-    datePaid: faker.date.between({ from: past, to: recently }),
-    credits: faker.number.int({ min: 0, max: 3 }),
-    checkoutSessionId: null,
-    subscriptionTier: faker.helpers.arrayElement([TierIds.HOBBY, TierIds.PRO]),
-  } satisfies Omit<User, 'id'>;
-  return user;
+    credits,
+    subscriptionStatus,
+    stripeId: hasUserPaidOnStripe ? `cus_test_${faker.string.uuid()}` : null,
+    datePaid: hasUserPaidOnStripe ? faker.date.between({ from: createdAt, to: lastActiveTimestamp }) : null,
+    checkoutSessionId: hasUserPaidOnStripe ? `cs_test_${faker.string.uuid()}` : null,
+    subscriptionTier: subscriptionStatus ? faker.helpers.arrayElement([TierIds.HOBBY, TierIds.PRO]) : null,
+  };
 }
