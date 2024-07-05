fea026f741cd9799a3ca486231fef79680ccdeb5
Ali Rastegar <68519335+VolVox99@users.noreply.github.com>
2024-07-05 03:17:12 -0700

    Update stripe.ts

--- template/app/src/server/webhooks/stripe.ts
+++ template/app/src/server/webhooks/stripe.ts
@@ -8,7 +8,7 @@ import Stripe from 'stripe';
 
 // make sure the api version matches the version in the Stripe dashboard
 const stripe = new Stripe(process.env.STRIPE_KEY!, {
-  apiVersion: '2022-11-15', // TODO find out where this is in the Stripe dashboard and document
+  apiVersion: '2022-11-15', // Find your version here - https://docs.stripe.com/development/dashboard/request-logs#view-your-default-api-version
 });
 
 export const stripeWebhook: StripeWebhook = async (request, response, context) => {
