f61e32c0b1111239275e0d77143120d9d38d9220
Mark Santolucito <mark.santolucito@yale.edu>
2024-10-17 21:08:57 -0400

    upgrade stripe version

--- template/app/src/payment/stripe/stripeClient.ts
+++ template/app/src/payment/stripe/stripeClient.ts
@@ -5,8 +5,10 @@ export const stripe = new Stripe(requireNodeEnvVar('STRIPE_API_KEY'), {
   // NOTE:
   // API version below should ideally match the API version in your Stripe dashboard.
   // If that is not the case, you will most likely want to (up/down)grade the `stripe`
-  // npm package to the API version that matches your Stripe dashboard's one.
+  // npm package to the API version  that matches your Stripe dashboard's one.
+  // This can be found in package.json, e.g. "stripe": "17.2.0"
+  // The latest version is always listed here: https://www.npmjs.com/package/stripe
   // For more details and alternative setups check
   // https://docs.stripe.com/api/versioning .
-  apiVersion: '2022-11-15',
+  apiVersion: '2024-09-30.acacia',
 });
