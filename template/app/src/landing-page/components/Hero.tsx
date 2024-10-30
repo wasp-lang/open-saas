387c6162acd883e7eca15eda19b715f74e252cb8
vincanger <70215737+vincanger@users.noreply.github.com>
2024-10-30 17:24:41 +0100

    fix hero

--- template/app/src/landing-page/components/Hero.tsx
+++ template/app/src/landing-page/components/Hero.tsx
@@ -4,9 +4,8 @@ import { DocsUrl } from '../../shared/common';
 export default function Hero() {
   return (
     <div className='relative pt-14 w-full'>
-      {/* <BackgroundGradient1 />
-      <BackgroundGradient2 /> */}
-
+      <TopGradient />
+      <BottomGradient />
       <div className='py-24 sm:py-32'>
         <div className='mx-auto max-w-8xl px-6 lg:px-8'>
           <div className='lg:mb-18 mx-auto max-w-3xl text-center'>
@@ -44,3 +43,34 @@ export default function Hero() {
   );
 }
 
+function TopGradient() {
+  return (
+    <div
+      className='absolute top-0 right-0 -z-10 transform-gpu overflow-hidden w-full blur-3xl sm:top-0'
+    aria-hidden='true'
+  >
+    <div
+      className='aspect-[1020/880] w-[55rem] flex-none sm:right-1/4 sm:translate-x-1/2 dark:hidden bg-gradient-to-tr from-amber-400 to-purple-300 opacity-40'
+      style={{
+        clipPath: 'polygon(80% 20%, 90% 55%, 50% 100%, 70% 30%, 20% 50%, 50% 0)',
+      }}
+      />
+    </div>
+  );
+}
+
+function BottomGradient() {
+  return (
+    <div
+      className='absolute inset-x-0 top-[calc(100%-40rem)] sm:top-[calc(100%-65rem)] -z-10 transform-gpu overflow-hidden blur-3xl'
+    aria-hidden='true'
+  >
+    <div
+      className='relative aspect-[1020/880] sm:-left-3/4 sm:translate-x-1/4 dark:hidden bg-gradient-to-br from-amber-400 to-purple-300  opacity-50 w-[72.1875rem]'
+      style={{
+        clipPath: 'ellipse(80% 30% at 80% 50%)',
+      }}
+    />
+    </div>
+  );
+}
