0ce8901d6950c0bcf6d2bf320feb05ea70e556de
vincanger <70215737+vincanger@users.noreply.github.com>
2024-11-15 16:07:47 +0100

    Update NavBar.tsx

--- template/app/src/client/components/NavBar/NavBar.tsx
+++ template/app/src/client/components/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ import { Dialog } from '@headlessui/react';
 import { BiLogIn } from 'react-icons/bi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { HiBars3 } from 'react-icons/hi2';
-import logo from '../static/logo.webp';
+import logo from '../../static/logo.webp';
 import DropdownUser from '../../../user/DropdownUser';
 import { UserMenuItems } from '../../../user/UserMenuItems';
 import DarkModeSwitcher from '../DarkModeSwitcher';
