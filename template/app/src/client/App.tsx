3d3796d81fa9d2bff0e3b2b2183aa52d9a728f82
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-02-13 17:15:12 +0100

    Update template to latest changes. Fixes user update bug

--- template/app/src/client/App.tsx
+++ template/app/src/client/App.tsx
@@ -8,7 +8,7 @@ import { routes } from 'wasp/client/router';
 import { Outlet, useLocation } from 'react-router-dom';
 import { useAuth } from 'wasp/client/auth';
 import { useIsLandingPage } from './hooks/useIsLandingPage';
-import { updateCurrentUser } from 'wasp/client/operations';
+import { updateCurrentUserLastActiveTimestamp } from 'wasp/client/operations';
 
 /**
  * use this component to wrap all child components
@@ -21,7 +21,9 @@ export default function App() {
   const navigationItems = isLandingPage ? landingPageNavigationItems : appNavigationItems;
 
   const shouldDisplayAppNavBar = useMemo(() => {
-    return location.pathname !== routes.LoginRoute.build() && location.pathname !== routes.SignupRoute.build();
+    return (
+      location.pathname !== routes.LoginRoute.build() && location.pathname !== routes.SignupRoute.build()
+    );
   }, [location]);
 
   const isAdminDashboard = useMemo(() => {
@@ -33,7 +35,7 @@ export default function App() {
       const lastSeenAt = new Date(user.lastActiveTimestamp);
       const today = new Date();
       if (today.getTime() - lastSeenAt.getTime() > 5 * 60 * 1000) {
-        updateCurrentUser({ lastActiveTimestamp: today });
+        updateCurrentUserLastActiveTimestamp({ lastActiveTimestamp: today });
       }
     }
   }, [user]);
