3038b55f531fd39dfe58873a25ff86cbbeef54e1
Filip Sodić <filip.sodic@fer.hr>
2025-02-25 20:11:20 +0100

    Remove app

--- opensaas-sh/app/migrations/20241126132514_remove_checkout_session_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `checkoutSessionId` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN IF EXISTS "checkoutSessionId";
--- opensaas-sh/app/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    open: true,
-    allowedHosts: true,
-  },
-});
