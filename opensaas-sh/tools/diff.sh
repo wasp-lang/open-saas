c75364929cb67eab80795c27f9b0d77e240b7ab5
Greg-CLD <Greg@confluity.co.uk>
2025-05-28 06:39:41 +0100

    Delete opensaas-sh directory

--- opensaas-sh/tools/diff.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-TOOLS_DIR=$(dirname "$(realpath "$0")")  # Assumes this script is in `tools/`.
-cd "${TOOLS_DIR}" && cd ../..
-
-rm -rf opensaas-sh/app_diff
-"${TOOLS_DIR}/dope.sh" template/app opensaas-sh/app diff
--- opensaas-sh/tools/patch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-TOOLS_DIR=$(dirname "$(realpath "$0")")  # Assumes this script is in `tools/`.
-cd "${TOOLS_DIR}" && cd ../..
-
-# Removes all files except for some gitignored files that we don't want to bother regenerating each time,
-# like node_modules and certain .env files.
-find opensaas-sh/app -mindepth 1 \( -path node_modules -o -name .env.server -o -name .env.me \) -prune -o -exec rm -rf {} +
-"${TOOLS_DIR}/dope.sh" template/app opensaas-sh/app patch
