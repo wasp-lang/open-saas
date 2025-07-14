aceb482646f79c5910eff141b943b1d79c470bc9
Fran Grgić <grgicfran@gmail.com>
2025-07-14 13:26:17 +0200

    add highlighted feature component

--- template/app/src/landing-page/components/HighlightedFeature.tsx
+++ template/app/src/landing-page/components/HighlightedFeature.tsx
@@ -8,6 +8,24 @@ interface FeatureProps {
   rotate?: boolean;
 }
 
+/**
+ * A component that highlights a feature with a description and a highlighted component.
+ * Shows text description on one side, and whatever component you want to show on the other side to demonstrate the functionality.
+ *
+ * @example
+ * <HighlightedFeature
+ *   name="Feature Name"
+ *   description="Feature description"
+ *   direction="row" // or "row-reverse"
+ *   highlightedComponent={<div>Highlighted Component</div>}
+ *   rotate={true} // or false
+ * />
+ * @param name - The name of the feature.
+ * @param description - The description of the feature.
+ * @param direction - The direction of the feature.
+ * @param highlightedComponent - The component to highlight.
+ * @param rotate - Whether to rotate the highlighted component.
+ */
 const HighlightedFeature = ({
   name,
   description,
@@ -20,7 +38,7 @@ const HighlightedFeature = ({
   return (
     <div
       className={cn(
-        'flex flex-col items-center my-50 gap-x-10 justify-between',
+        'flex flex-col items-center my-50 gap-x-20 justify-between px-8 md:px-0',
         direction === 'row' ? 'md:flex-row' : 'md:flex-row-reverse'
       )}
     >
