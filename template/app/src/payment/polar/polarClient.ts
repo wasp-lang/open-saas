549d3ea6cf145f4084e9fcfb81e5667c7e87dd6b
Franjo Mindek <franjo.mindek@gmail.com>
2025-11-19 12:14:14 +0100

    temp changes save

--- template/app/src/payment/polar/polarClient.ts
+++ template/app/src/payment/polar/polarClient.ts
@@ -2,7 +2,7 @@ import { Polar } from "@polar-sh/sdk";
 import { requireNodeEnvVar } from "../../server/utils";
 
 export const polarClient = new Polar({
-  accessToken: requireNodeEnvVar("POLAR_ACCESS_TOKEN"),
+  accessToken: requireNodeEnvVar("POLAR_ORGANIZATION_ACCESS_TOKEN"),
   server:
     requireNodeEnvVar("POLAR_SANDBOX_MODE") === "true"
       ? "sandbox"
