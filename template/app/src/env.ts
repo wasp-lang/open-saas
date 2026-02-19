8ed119ba4231e8a11d01870b3fa12fca8cf8661e
Mihovil Ilakovac <mihovil@ilakovac.com>
2026-02-19 15:31:17 +0100

    Split plans into server-only file and cross-runtime file

--- template/app/src/env.ts
+++ template/app/src/env.ts
@@ -1,20 +1,20 @@
 import { defineEnvValidationSchema } from 'wasp/env'
+
 import { authEnvSchema } from './auth/env'
-import { stripeEnvSchema } from './payment/env'
-// import { lemonSqueezyEnvSchema } from './payment/env'
-// import { polarEnvSchema } from './payment/env'
+import { stripeEnvSchema } from './payment/stripe/env'
+import { lemonSqueezyEnvSchema } from './payment/lemonSqueezy/env'
+import { polarEnvSchema } from './payment/polar/env'
 import { demoAiAppEnvSchema } from './demo-ai-app/env'
 import { fileUploadEnvSchema } from './file-upload/env'
-import { plausibleEnvSchema } from './analytics/env'
-// import { googleAnalyticsEnvSchema } from './analytics/env'
+import { plausibleEnvSchema, googleAnalyticsEnvSchema } from './analytics/env'
 
 export const serverEnvValidationSchema = defineEnvValidationSchema(
   authEnvSchema
     .merge(stripeEnvSchema)
-    // .merge(lemonSqueezyEnvSchema)
-    // .merge(polarEnvSchema)
+    .merge(lemonSqueezyEnvSchema)
+    .merge(polarEnvSchema)
     .merge(demoAiAppEnvSchema)
     .merge(fileUploadEnvSchema)
     .merge(plausibleEnvSchema)
-    // .merge(googleAnalyticsEnvSchema)
+    .merge(googleAnalyticsEnvSchema)
 )
