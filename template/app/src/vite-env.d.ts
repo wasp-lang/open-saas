defdd5875350224f79ddab3570b66c755a008735
Carlos Precioso <carlos@wasp-lang.dev>
2026-04-16 10:24:34 +0200

    Add ts-reset

--- template/app/src/vite-env.d.ts
+++ template/app/src/vite-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 
+// These add some tweaks to the types of common JavaScript built-in objects and
+// DOM APIs to make their usage more ergonomic and type-safe.
+import "@total-typescript/ts-reset";
+import "@total-typescript/ts-reset/dom";
+
 // This is needed to properly support Vitest testing with jest-dom matchers.
 // Types for jest-dom are not recognized automatically and Typescript complains
 // about missing types e.g. when using `toBeInTheDocument` and other matchers.
